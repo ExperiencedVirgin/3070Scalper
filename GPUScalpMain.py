dc4d5cae3760e7e101cd77385001af77f1cf7064
ExperiencedVirgin <81113527+ExperiencedVirgin@users.noreply.github.com>
2021-03-23 18:05:44 -0700

    Update GPUScalpMain.py

--- GPUScalpMain.py
+++ GPUScalpMain.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
@@ -23,19 +25,44 @@ while(Available != True):
         time.sleep(2)
         travis.refresh()
     except:
-
         BuyGPU = travis.find_element_by_class_name("btn-primary")
         print(travis.title)
         print("In Stock Purchasing")
-        time.sleep(3)
+        time.sleep(2)
         BuyGPU.click()
         Available = True
-travis.get("https://www.bestbuy.com/cart")
-GoToCart = travis.find_element_by_class_name("btn-primary")
-GoToCart.click()
-travis.get("https://www.bestbuy.com/identity/signin?token=tid%3A256344d2-8a88-11eb-9fba-005056aeda70")
-GoAsGuest = travis.find_element_by_class_name("btn-lg")
-GoAsGuest.click()
+
+Cart = travis.get("https://www.bestbuy.com/cart")
+GoToCheckout = travis.find_element_by_class_name("btn-primary").click()
+
+try:
+    LoginPage = WebDriverWait(travis, 10).until(
+        EC.presence_of_element_located((By.ID, "fld-e"))
+    )
+    usrnme = travis.find_element_by_id("fld-e")
+    usrnme.send_keys("getscammed42069@gmail.com")
+    psswrd = travis.find_element_by_id("fld-p1")
+    psswrd.send_keys("Doublebub@21")
+    SignIn = travis.find_element_by_class_name("btn-secondary").click()
+    time.sleep(3)
+    ContToPayment = travis.find_element_by_class_name("btn-secondary").click()
+    try:
+        LoginPage = WebDriverWait(travis, 10).until(
+            EC.presence_of_element_located((By.ID, "optimized-cc-card-number"))
+        )
+        card = travis.find_elements_by_id("optimized-cc-card-number").send_keys("4829512674")
+        firstname = travis.find_element_by_id("payment.billingAddress.firstName").send_keys("Travis")
+        lastname = travis.find_element_by_id("payment.billingAddress.lastName").send_keys("Dingleberry")
+        address = travis.find_element_by_id("payment.billingAddress.street").send_keys("1125 West Grove ST")
+    except:
+        print("Issue with Payment")
+        travis.quit()
+except:
+    print("Error on Login")
+    travis.quit()
+
+
+
 
 
 
