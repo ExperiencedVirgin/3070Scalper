a46757a080c80778712f29d1fb4790aaf854ea17
ExperiencedVirgin <81113527+ExperiencedVirgin@users.noreply.github.com>
2021-03-28 21:02:32 -0700

    Main Version Done

--- GPUScalpMain.py
+++ GPUScalpMain.py
@@ -1,68 +1,104 @@
+from tkinter import *
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-###Location of webdriver.exe
-PATH = "C:\Program Files (x86)\ChromeDriver\chromedriver.exe"
-###URL to download chrome driver
-###(https://sites.google.com/a/chromium.org/chromedriver/downloads)
-travis = webdriver.Chrome(PATH)
+#Timing purchase
+#start_time = time.time()
 
-#travis.get("https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442")
-###Test Study
-travis.get("https://www.bestbuy.com/site/pny-geforce-gt1030-2gb-pci-e-3-0-graphics-card-black/5901353.p?skuId=5901353")
-Available = False
-OutOfStock = True
+#Takes info from submit button and passes to main program
+def click():
+    scalp_bot_3000(bestbuy_link.get(),email_pypal.get(),password_pypal.get())
 
-while(Available != True):
-    try:
-        BuyGPU = travis.find_element_by_class_name("btn-disabled")
-        print(travis.title)
-        print("Out of Stock")
-        time.sleep(2)
-        travis.refresh()
-    except:
-        BuyGPU = travis.find_element_by_class_name("btn-primary")
-        print(travis.title)
-        print("In Stock Purchasing")
-        time.sleep(2)
-        BuyGPU.click()
-        Available = True
+#GUI Setup
+app = Tk()
+app.geometry("700x700")
+app.title("Best Buy Scalper")
+app.configure(background="black")
+gpu_photo = PhotoImage(file="N6SqRbVmDQzZWQMDjtntrW.png")
+gpu_photo.subsample(2, 2)
+gpu_label = Label (app, image=gpu_photo, bg="black").grid(row=0,column=0,columnspan=2,rowspan=1,padx=3,pady=3)
 
-Cart = travis.get("https://www.bestbuy.com/cart")
-GoToCheckout = travis.find_element_by_class_name("btn-primary").click()
-
-try:
-    LoginPage = WebDriverWait(travis, 10).until(
-        EC.presence_of_element_located((By.ID, "fld-e"))
-    )
-    usrnme = travis.find_element_by_id("fld-e")
-    usrnme.send_keys("getscammed42069@gmail.com")
-    psswrd = travis.find_element_by_id("fld-p1")
-    psswrd.send_keys("Doublebub@21")
-    SignIn = travis.find_element_by_class_name("btn-secondary").click()
-    time.sleep(3)
-    ContToPayment = travis.find_element_by_class_name("btn-secondary").click()
-    try:
-        LoginPage = WebDriverWait(travis, 10).until(
-            EC.presence_of_element_located((By.ID, "optimized-cc-card-number"))
-        )
-        card = travis.find_elements_by_id("optimized-cc-card-number").send_keys("4829512674")
-        firstname = travis.find_element_by_id("payment.billingAddress.firstName").send_keys("Travis")
-        lastname = travis.find_element_by_id("payment.billingAddress.lastName").send_keys("Dingleberry")
-        address = travis.find_element_by_id("payment.billingAddress.street").send_keys("1125 West Grove ST")
-    except:
-        print("Issue with Payment")
-        travis.quit()
-except:
-    print("Error on Login")
-    travis.quit()
+#Widget Declaring
+bestbuy_link_form = Label(app,text="Enter Best Buy Link",bg="black",fg="white",font="none 12 bold")
+bestbuy_link = Entry(app,width=50)
+email_text_form = Label (app,text="Enter Paypal Email", bg="black", fg="white", font="none 12 bold")
+email_pypal = Entry(app,width=50)
+password_text_form = Label(app,text="Enter Paypal Password", bg="black", fg="white", font="none 12 bold")
+password_pypal = Entry(app,width=50)
+submit_button = Button(app, text="SUBMIT", width=8, command=click)
 
 
+#Label GUI Setup
+bestbuy_link_form.grid(row=1,column=0)
+bestbuy_link.grid(row=1,column=1)
+email_text_form.grid(row=2,column=0)
+email_pypal.grid(row=2,column=1)
+password_text_form.grid(row=3,column=0)
+password_pypal.grid(row=3,column=1)
+submit_button.grid(row=5,column=1)
 
+#Main Function of scalp bot
+def scalp_bot_3000(link,pyplemail,pyplpw):
+    # Location of webdriver.exe
+    PATH = "C:\Program Files (x86)\ChromeDriver\chromedriver.exe"
+    #URL to download chrome driver
+    #(https://sites.google.com/a/chromium.org/chromedriver/downloads)
+    travis = webdriver.Chrome(PATH)
 
+    travis.get(link)
+    #Test Link
+    #travis.get("https://www.bestbuy.com/site/logitech-m325-wireless-optical-mouse-works-with-chromebook-silver/2577677.p?skuId=2577677")
+    Available = False
 
+    while(Available != True):
+        try:
+            buy_item = travis.find_element_by_class_name("btn-disabled")
+            print(travis.title)
+            print("Out of Stock")
+            travis.implicitly_wait(1)
+            travis.refresh()
+        except:
+            buy_item = travis.find_element_by_class_name("btn-primary")
+            print(travis.title)
+            print("In Stock Purchasing")
+            travis.implicitly_wait(1)
+            buy_item.click()
+            Available = True
 
+        bestbuy_cart = travis.get("https://www.bestbuy.com/cart")
+        #Wait for BestBuy Paypal Purchase Button to Load
+        try:
+            bestbuy_checkout = WebDriverWait(travis, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "checkout-buttons__paypal"))
+            )
+            bestbuy_checkout.click()
+            #Wait for Paypal Email Prompt to Load
+            try:
+                paypal_email_textbox = WebDriverWait(travis, 10).until(
+                    EC.presence_of_element_located((By.ID, "email"))
+                )
+                paypal_email_textbox.send_keys(pyplemail)
+                travis.find_element_by_id("btnNext").click()
+                travis.implicitly_wait(1)
+                paypal_password_textbox = travis.find_element_by_xpath('//*[@id="password"]')
+                paypal_password_textbox.send_keys(pyplpw)
+                login_paypal = travis.find_element_by_id("btnLogin")
+                login_paypal.click()
+                continue_paypal = travis.find_element_by_id("payment-submit-btn")
+                continue_paypal.click()
+                placeorder = travis.find_element_by_class_name("btn-primary")
+                placeorder.click()
+                print("Order successful")
+                #print("%s seconds" % (time.time()-start_time))
+                travis.quit()
+            except:
+                print("Error on Login")
+                travis.quit()
+        except:
+            print("Error during checkout")
+            travis.quit()
+
+app.mainloop()
